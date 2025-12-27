8f79e2c589432b41fc193cff8626f8397e30deff
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-12-27 11:11:06 +0700

    feat: Add skin interaction

---
+++ Assets/Scripts/Scriptables/Interaction/SkinInteractionLogic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Interaction/Logic/Skin")]
+public class SkinInteractionLogic : InteractionLogic
+{
+    public SkinData skinData;
+
+    public override void Execute(InteractionContext ctx)
+    {
+        if (skinData == null)
+            return;
+
+        ctx.Model.skinController.SetSkin(skinData);
+    }
+
+    public override void End(InteractionContext ctx)
+    {
+        return;
+    }
+}
