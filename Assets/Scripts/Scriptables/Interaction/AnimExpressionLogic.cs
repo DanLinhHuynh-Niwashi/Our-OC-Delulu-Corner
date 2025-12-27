56ee2092d80c5f0758ce36f6db7993f2b81b7791
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-12-27 21:39:36 +0700

    feat: eyeblink control on anim only

--- Assets/Scripts/Scriptables/Interaction/AnimExpressionLogic.cs
+++ Assets/Scripts/Scriptables/Interaction/AnimExpressionLogic.cs
@@ -8,28 +8,59 @@ public class AnimExpressionLogic : InteractionLogic
 {
     public List<TargetAnimation> targetAnims;
     public int targetExpression = 0;
+
+    private bool eyeBlinkDisabled = false;
     public override void Execute(InteractionContext ctx)
     {
         ctx.Model.modelMotionController.OnInteraction(targetAnims, targetExpression);
+
+        if (ctx.Model.eyeBlinkController && ctx.Model.eyeBlinkController.enabled)
+        {
+            ctx.Model.eyeBlinkController.enabled = false;
+            eyeBlinkDisabled = true;
+        }
     }
 
     public override void End(InteractionContext ctx)
     {
         ctx.Model.modelMotionController.ForceStopAll();
         ctx.Model.modelMotionController.PlayDefaultState();
+
+        RestoreEyeBlink(ctx);
     }
 
     public override bool IsBusy(InteractionContext ctx)
     {
+        bool isPlaying = false;
+
         foreach (var anim in targetAnims)
         {
             if (ctx.Model.modelMotionController.IsPlayingAnimation(anim.layer))
-                return true;
+            {
+                isPlaying = true;
+                break;
+            }
         }
-        return false;
+
+        if (!isPlaying && eyeBlinkDisabled)
+        {
+            RestoreEyeBlink(ctx);
+        }
+
+        return isPlaying;
     }
 
+    private void RestoreEyeBlink(InteractionContext ctx)
+    {
+        if (!eyeBlinkDisabled) return;
+
+        if (ctx.Model.eyeBlinkController)
+            ctx.Model.eyeBlinkController.enabled = true;
+
+        eyeBlinkDisabled = false;
+    }
 }
+
 
 [System.Serializable]
 public struct TargetAnimation
